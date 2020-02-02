9ada700c427a54d149d0325f4db324ae649d4131
FreesiaLikesPomelo <akeywang@foxmail.com>
2020-02-02 15:11:39 +0800

    Update replaceSpace.py

--- replaceSpace.py
+++ replaceSpace.py
@@ -1,9 +1,8 @@
-# -*- coding: UTF-8 -*- 
 
 class Solution:
     def replaceSpace(self, s):
         # write code here
-        # return s.replace(' ','%20') # 
+        # return s.replace(' ','%20') # then it's done
         spaceCount = 0
         #print(len(s))
         if(len(s)==0):
@@ -18,25 +17,22 @@ class Solution:
         s = list(s)
         #print(len(s))
         while(spaceCount>0):
-			#print(p_tail," ",p_fwd)
-			if(s[p_fwd]==' '):
-				#s=s.replace(s[p_tail-2:p_tail],'%20')
-				#print(p_tail," ",p_fwd)
-				#s[p_tail-2:p_tail]='%20'
-				s[p_tail]='0'
-				s[p_tail-1]='2'
-				s[p_tail-2]='%'
-				#print(s)
-				p_tail-=3
-				p_fwd-=1
-				spaceCount -=1
-			else:
-				s[p_tail]=s[p_fwd]
-			    #print(s)
-				p_tail-=1
-				p_fwd-=1
+            if(s[p_fwd]==' '):
+                #s=s.replace(s[p_tail-2:p_tail],'%20') # str.replace('x','O') will replace all 'x' in str. 
+                #s[p_tail-2:p_tail]='%20' # the index doesn't need to be checked... it might extend the list
+                s[p_tail]='0'
+                s[p_tail-1]='2'
+                s[p_tail-2]='%'
+                p_tail-=3
+                p_fwd-=1
+                spaceCount -=1
+            else:
+                s[p_tail]=s[p_fwd]
+                p_tail-=1
+                p_fwd-=1
         return ''.join(s)
     
+
 '''
 # below is an example. 
 sol=Solution()
